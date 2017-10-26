cc084e0a12f444c0962cba31ba1674dff6959176
xankraegor <xankraegor@xankraegor.com>
2017-10-27 02:10:47 +0300

    Refactored Bill_ class

--- RussianBills/Bill.swift
+++ RussianBills/Bill.swift
@@ -10,7 +10,7 @@ import Foundation
 import RealmSwift
 import SwiftyJSON
 
-/// Законопроект
+
 final class Bill_: Object, InitializableWithJson {
 
     @objc dynamic var id: Int = 0
@@ -23,14 +23,10 @@ final class Bill_: Object, InitializableWithJson {
     @objc dynamic var transcriptUrl: String = ""
     @objc dynamic var favorite: Bool = false
 
-    /* Introduced by: */
     let factions = List<Factions_>()
     let deputees = List<Deputy_>()
     let federalSubjects = List<FederalSubject_>()
     let regionalSubjects = List<RegionalSubject_>()
-    /*
-     Duma deputies and members of the Federal Council, ferderal and regional subjects of legislative initiative
-     */
 
     @objc dynamic var lastEventStage: Stage_?
     @objc dynamic var lastEventPhase: Phase_?
@@ -53,8 +49,7 @@ final class Bill_: Object, InitializableWithJson {
 
     internal convenience required init(withJson json: JSON) {
         self.init()
-
-        // Basic variables ==============================================================================
+        // Basic values
         id = json["id"].intValue
         lawType = LawType(rawValue: json["type"]["id"].intValue)!
         name = json["name"].stringValue
@@ -63,10 +58,26 @@ final class Bill_: Object, InitializableWithJson {
         introductionDate = json["introductionDate"].stringValue
         url = json["url"].stringValue
         transcriptUrl = json["transcriptUrl"].stringValue
+        // Compound values
+        decodeFactions(json)
+        decodeDeputees(json)
+        decodeOtherSubjects(json)
+        decodeResponsibleCommittees(json)
+        decodeProfileCommittees(json)
+        decodeCoexecutors(json)
+        decodeStages(json)
+        decodePhases(json)
+        decodeResolution(json)
+    }
 
-        // Factions ==============================================================================
+    override static func primaryKey() -> String {
+        return "number"
+    }
+
+    // Initialization functions
+
+    func decodeFactions(_ json: JSON) {
         let factions = json["subject"]["factions"].arrayValue
-        
         for fac in factions {
             let facId = fac["id"].intValue
             if let faction = RealmCoordinator.loadObject(Factions_.self, byId: facId) {
@@ -79,10 +90,10 @@ final class Bill_: Object, InitializableWithJson {
                 self.factions.append(faction)
             }
         }
+    }
 
-        // Deputies ==============================================================================
+    func decodeDeputees(_ json: JSON) {
         let deputies = json["subject"]["deputies"].arrayValue
-
         for dep in deputies {
             let depId = dep["id"].intValue
             if let deputy = RealmCoordinator.loadObject(Deputy_.self, byId: depId) {
@@ -93,15 +104,14 @@ final class Bill_: Object, InitializableWithJson {
                 deputy.name = dep["name"].stringValue
                 deputy.isCurrent = dep["isCurrent"].boolValue
                 deputy.position = dep["position"].stringValue
-
                 RealmCoordinator.save(object: deputy)
                 deputees.append(deputy)
             }
         }
+    }
 
-        // Other subjects ==============================================================================
+    func decodeOtherSubjects(_ json: JSON) {
         let subjects = json["subject"]["departments"].arrayValue
-
         for sub in subjects {
             let subId = sub["id"].intValue
 
@@ -115,8 +125,9 @@ final class Bill_: Object, InitializableWithJson {
                 debugPrint("∆ FED/REG SUBJECT NOT FOUND IN DB: \n\(sub)\n===================================================")
             }
         }
+    }
 
-        // Responsible committee ==============================================================================
+    func decodeResponsibleCommittees(_ json: JSON) {
         let comitteeId = json["committees"]["responsible"]["id"].intValue
         if let committeeById = RealmCoordinator.loadObject(Comittee_.self, byId: comitteeId) {
             comitteeResponsible = committeeById
@@ -127,12 +138,12 @@ final class Bill_: Object, InitializableWithJson {
             committee.isCurrent = json["committees"]["responsible"]["isCurrent"].boolValue
             committee.startDate = json["committees"]["responsible"]["startDate"].stringValue
             committee.stopDate = json["committees"]["responsible"]["stopDate"].stringValue
-
             RealmCoordinator.save(object: committee)
             comitteeResponsible = committee
         }
+    }
 
-        // Profile committees ==============================================================================
+    func decodeProfileCommittees(_ json: JSON) {
         let profileComs = json["committees"]["profile"].arrayValue
 
         for com in profileComs {
@@ -151,8 +162,9 @@ final class Bill_: Object, InitializableWithJson {
                 comitteeProfile.append(committee)
             }
         }
+    }
 
-        // Coexexutor committees ==============================================================================
+    func decodeCoexecutors(_ json: JSON) {
         let coexecs = json["committees"]["soexecutor"].arrayValue
 
         for com in coexecs {
@@ -171,8 +183,9 @@ final class Bill_: Object, InitializableWithJson {
                 comitteeCoexecutor.append(committee)
             }
         }
+    }
 
-        // Stage ==============================================================================
+    func decodeStages(_ json: JSON) {
         let lastEventStageId = json["lastEvent"]["stage"]["id"].intValue
         if let stage = RealmCoordinator.loadObject(Stage_.self, byId: lastEventStageId) {
             lastEventStage = stage
@@ -184,8 +197,9 @@ final class Bill_: Object, InitializableWithJson {
             RealmCoordinator.save(object: stage)
             lastEventStage = stage
         }
+    }
 
-        // Phase ==============================================================================
+    func decodePhases(_ json: JSON) {
         let lastEventPhaseId = json["lastEvent"]["phase"]["id"].intValue
         if let phase = RealmCoordinator.loadObject(Phase_.self, byId: lastEventPhaseId) {
             lastEventPhase = phase
@@ -197,16 +211,13 @@ final class Bill_: Object, InitializableWithJson {
             RealmCoordinator.save(object: phase)
             lastEventPhase = phase
         }
+    }
 
-        // Solution ==============================================================================
+    func decodeResolution(_ json: JSON) {
         lastEventSolutionDescription = json["lastEvent"]["solution"].stringValue
         lastEventDate = json["lastEvent"]["date"].stringValue
         lastEventDocumentName = json["lastEvent"]["document"]["name"].stringValue
         lastEventDocumentType = json["lastEvent"]["document"]["type"].stringValue
-    }
-
-    override static func primaryKey() -> String {
-        return "number"
     }
 
     // MARK: - Helper functions
