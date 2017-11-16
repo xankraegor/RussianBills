e85a16d09539c799abea87ed4c09d4d5ecc34457
xankraegor <xankraegor@xankraegor.com>
2017-11-16 20:15:20 +0300

    Added Legislative Subject enumeration

---
+++ RussianBills/LegislativeSubjectTypeEnum.swift
@@ -0,0 +1,35 @@
+//
+//  LegislativeSubjectTypeEnum.swift
+//  RussianBills
+//
+//  Created by Xan Kraegor on 16.11.2017.
+//  Copyright © 2017 Xan Kraegor. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+enum LegislativeSubjectType {
+    case federalSubject
+    case regionalSubject
+    case deputy
+
+    private func objectType()->Object.Type {
+        switch self {
+        case .federalSubject:
+            return FederalSubject_.self
+        case .regionalSubject:
+            return RegionalSubject_.self
+        case .deputy:
+            return Deputy_.self
+        }
+    }
+
+    func item(byId id: Int)->Object? {
+        if let realm = try? Realm() {
+            return realm.object(ofType: objectType(), forPrimaryKey: id)
+        }
+
+        return nil
+    }
+}
