cd0680cfd019b382112d7b14d00fc386fdeb1b5e
xankraegor <xankraegor@xankraegor.com>
2017-12-05 00:48:48 +0300

    Added basic Watch OS interfaces and target

---
+++ RusBillsWatchApp Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RusBillsWatchApp Extension
+//
+//  Created by Xan Kraegor on 04.12.2017.
+//  Copyright © 2017 Xan Kraegor. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
